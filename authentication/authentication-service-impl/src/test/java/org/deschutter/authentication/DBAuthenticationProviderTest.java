d8c034c9591c6bad80f9a4acd3ca9f88ef3092f6
Berten <Berten>
2014-02-11 15:03:18 +0100

    Unit Tests

---
+++ authentication/authentication-service-impl/src/test/java/org/deschutter/authentication/DBAuthenticationProviderTest.java
@@ -0,0 +1,80 @@
+package org.deschutter.authentication;
+
+import org.deschutter.user.User;
+import org.deschutter.user.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.TestCase.assertFalse;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DBAuthenticationProviderTest {
+
+	public static final String USERNAME = "username";
+	public static final String PASSWORD = "password";
+	@InjectMocks
+	private DBAuthenticationProvider authenticationProvider;
+
+	@Mock
+	private UserRepository userRepository;
+
+	private User user;
+	private TestingAuthenticationToken authentication;
+
+	@Before
+	public void setUp() throws Exception {
+		user = new User(USERNAME, PASSWORD);
+		when(userRepository.findByUsernameAndPassword(USERNAME, PASSWORD)).thenReturn(user);
+		authentication = new TestingAuthenticationToken(USERNAME, PASSWORD);
+	}
+
+	@Test
+	public void authenticate_HasUser() throws Exception {
+		Authentication authentication = authenticationProvider.authenticate(this.authentication);
+		assertEquals(user, authentication.getPrincipal());
+	}
+
+	@Test
+	public void authenticate_HasCredentials() throws Exception {
+		Authentication authentication = authenticationProvider.authenticate(this.authentication);
+		assertEquals(this.authentication, authentication.getCredentials());
+	}
+
+	@Test
+	public void authenticate_HasAuthority_USER() throws Exception {
+		Authentication authentication = authenticationProvider.authenticate(this.authentication);
+		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		assertEquals(1, authorities.size());
+		assertEquals("USER", authorities.iterator().next().getAuthority());
+	}
+
+	@Test(expected = BadCredentialsException.class)
+	public void authenticate_DoesNotFindUser_ThrowsException() {
+		when(userRepository.findByUsernameAndPassword(USERNAME, PASSWORD)).thenReturn(null);
+		authenticationProvider.authenticate(authentication);
+	}
+
+	@Test
+	public void supports_DoesNotSupportTestingAuthentication() {
+		assertFalse(authenticationProvider.supports(TestingAuthenticationToken.class));
+	}
+
+	@Test
+	public void supports_SupportsusernamePasswordToken() {
+		assertTrue(authenticationProvider.supports(UsernamePasswordAuthenticationToken.class));
+	}
+}
