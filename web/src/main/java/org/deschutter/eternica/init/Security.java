30ee7b07f429cb2cfccad0c9fadd37b9b5a73fa0
Berten <Berten>
2014-02-11 17:32:23 +0100

    Controller Testing

--- web/src/main/java/org/deschutter/eternica/init/Security.java
+++ web/src/main/java/org/deschutter/eternica/init/Security.java
@@ -3,16 +3,33 @@ package org.deschutter.eternica.init;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 @Configuration
 @EnableWebSecurity
-@ComponentScan(basePackages = {"org.deschutter"})
+@ComponentScan(basePackages = {"org.deschutter"},excludeFilters =  {@ComponentScan.Filter(
+        value= Repository.class,
+        type = FilterType.ANNOTATION
+),@ComponentScan.Filter(
+        value= Controller.class,
+        type = FilterType.ANNOTATION
+),@ComponentScan.Filter(
+        value= Component.class,
+        type = FilterType.ANNOTATION
+),@ComponentScan.Filter(
+        value= Service.class,
+        type = FilterType.ANNOTATION
+)})
 public class Security extends WebSecurityConfigurerAdapter {
     @Autowired
     private AuthenticationProvider authenticationProvider;
